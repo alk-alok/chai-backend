95e632f2b94e22913d68e4c24126cc7f7d8232c5
Alok Kumar <kumaralok47ak@gmail.com>
2024-04-21 19:11:12 +0530

    All setup done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";// ye isliye use karte hai jisse ham user ke brwser ki cokkee access kar paye aur unme kuchh chnages kar paye-> basically CRUD operations kar paye
 
+
 const app = express();
 
 app.use(cors({
@@ -13,5 +14,13 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export {app};
